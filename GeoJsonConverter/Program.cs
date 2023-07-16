df351982a2185d6470ba0e5aaa1b6d4e7bf6909d
阿峰 <a2248817@gmail.com>
2023-07-16 19:31:17 +0800

    加入專案檔案。

---
+++ GeoJsonConverter/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Syncfusion.Blazor;
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSyncfusionBlazor();
+builder.Services.AddHttpClient();
+
+var app = builder.Build();
+
+var key = app.Configuration["SyncfusionLicenseKey"];
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense(key);
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
+
---
+++ GeoJsonHelper/Utilities/GeoJsonHelper.cs
@@ -0,0 +1,46 @@
+﻿using GeoJSON.Text.Feature;
+using GeoJSON.Text.Geometry;
+using System.Text.Json;
+
+namespace GeoJsonHelper.Utilities;
+
+public class GeoJsonHelper
+{
+    public static FeatureCollection CreateGrid(int x, int y)
+    {
+        var result = new FeatureCollection();
+
+        for (int i = 0; i < y; i++)
+        {
+            for (int j = 0; j < x; j++)
+            {
+                var feature = new Feature();
+                //latitude 緯度 Y, longitude 經度 X 
+                var position = new Position(i, j);
+                var geometry = new Point(position);
+                feature.Geometry = geometry;
+                feature.Id = $"{i * x + j}";
+                var properties = new Dictionary<string, object>();
+                properties.Add("x", j);
+                properties.Add("y", i);
+                feature.Properties = properties;
+                result.Features.Add(feature);
+            }
+        }
+
+        return result;
+    }
+
+    public static async Task ToGeoJsonFileAsync(FeatureCollection collection, string filePath)
+    {
+        var geoJson = ToGeoJson(collection);
+        await File.WriteAllTextAsync(filePath, geoJson);
+    }
+
+    public static string ToGeoJson(FeatureCollection collection)
+    {
+        var geoJson = JsonSerializer.Serialize(collection, new JsonSerializerOptions { WriteIndented = true });
+        return geoJson;
+    }
+
+}
