5314e2ae0ef1c64443a4919b0b93b68559561993
阿峰 <a2248817@gmail.com>
2023-07-18 00:54:40 +0800

    新增刪除檔案按鈕 選取Point功能WIP
    預計
    完成選取Point功能
    建立Polygon、LineString功能

--- GeoJsonHelper/Utilities/GeoJsonHelper.cs
+++ GeoJsonHelper/Utilities/GeoJsonHelper.cs
@@ -6,30 +6,99 @@ namespace GeoJsonHelper.Utilities;
 
 public class GeoJsonHelper
 {
-    public static FeatureCollection CreateGrid(int x, int y)
+    public static Feature CreatePoint(int x, int y)
+    {
+        var feature = new Feature();
+
+        //latitude 緯度 Y, longitude 經度 X 
+        var position = new Position(y, x);
+
+        var point = new Point(position);
+        feature.Geometry = point;
+        feature.Id = $"{Guid.NewGuid()}";
+
+        var properties = new Dictionary<string, object>();
+        properties.Add("guid", feature.Id);
+        properties.Add("x", x);
+        properties.Add("y", y);
+        feature.Properties = properties;
+
+        return feature;
+    }
+    public static FeatureCollection CreatePoints(int x, int y)
     {
         var result = new FeatureCollection();
 
+        //再往上建立Y方向點
         for (int i = 0; i < y; i++)
         {
+            //先往右建立X方向點
             for (int j = 0; j < x; j++)
             {
-                var feature = new Feature();
-                //latitude 緯度 Y, longitude 經度 X 
-                var position = new Position(i, j);
-                var geometry = new Point(position);
-                feature.Geometry = geometry;
-                feature.Id = $"{i * x + j}";
-                var properties = new Dictionary<string, object>();
-                properties.Add("x", j);
-                properties.Add("y", i);
-                feature.Properties = properties;
+                var feature = GeoJsonHelper.CreatePoint(j, i);
                 result.Features.Add(feature);
             }
         }
 
         return result;
     }
+
+    public static Feature CreateLineString(IEnumerable<Feature> points)
+    {
+        var feature = new Feature();
+
+        var positions = new List<IPosition>();
+        var guids = new List<string>();
+        foreach (var point in points)
+        {
+            var position = ((Point)point.Geometry).Coordinates;
+            positions.Add(position);
+            guids.Add(point.Id);
+        }
+        //latitude 緯度 Y, longitude 經度 X 
+
+        var lineString = new LineString(positions);
+
+        feature.Geometry = lineString;
+        feature.Id = $"{Guid.NewGuid()}";
+
+        var properties = new Dictionary<string, object>();
+        properties.Add("guid", feature.Id);
+        properties.Add("guids", String.Join(" ",guids));
+        feature.Properties = properties;
+
+        return feature;
+    }
+
+    public static Feature CreateRectangle(int x, int y)
+    {
+        var feature = new Feature();
+
+        //latitude 緯度 Y, longitude 經度 X 
+        var positions = new List<IPosition>();
+
+        positions.Add(new Position(0, 0));
+        positions.Add(new Position(0, x));
+        positions.Add(new Position(y, x));
+        positions.Add(new Position(y, 0));
+        positions.Add(new Position(0, 0));
+
+        var lineString = new LineString(positions);
+
+        var rectangle = new Polygon(new List<LineString> { lineString });
+
+        feature.Geometry = rectangle;
+        feature.Id = $"{Guid.NewGuid()}";
+
+        var properties = new Dictionary<string, object>();
+        properties.Add("guid", feature.Id);
+        properties.Add("x", x);
+        properties.Add("y", y);
+        feature.Properties = properties;
+
+        return feature;
+    }
+
 
     public static async Task ToGeoJsonFileAsync(FeatureCollection collection, string filePath)
     {
